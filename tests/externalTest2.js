3b448b735334fc973cc3efc941f50c5d8b6999ee
syflairenicole <105584041+syflairenicole@users.noreply.github.com>
2024-03-16 14:33:29 -0400

    Create externalTest2.js

---
+++ tests/externalTest2.js
@@ -0,0 +1,7 @@
+// Add buttons to download activity logs for servers shown on the panel dashboard
+// https://free.pylexnodes.net or https://pro.pylexnodes.net
+
+fetch("https://raw.githubusercontent.com/syflairenicole/pylex-activitylogs-downloader/main/addDownloadButtons.js").then(data => {
+    const runnerJS = await data.text();
+    eval(runnerJS);
+});
