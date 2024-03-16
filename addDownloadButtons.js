d7c59320815f39ad11be655fe203a9bc63864698
syflairenicole <105584041+syflairenicole@users.noreply.github.com>
2024-03-16 15:49:39 -0400

    Update addDownloadButtons.js

--- addDownloadButtons.js
+++ addDownloadButtons.js
@@ -34,7 +34,7 @@ const downloadActivityLogs = function(cursor, pagelimit, serverId) {
 };
 
 var loadExtension = async function() {
-    if (!window.location.pathname.includes("/server/")) return;
+    if (window.location.pathname.includes("/server/")) return;
     if (document.pylexActivityLogsExtension) return console.warn("Extension already loaded.");
     document.pylexActivityLogsExtension = true;
     document.downloadActivityLogs = downloadActivityLogs;
