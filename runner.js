9b12665ff18ae1fb57e3fc7c5b0c1ab48cef4b01
syflairenicole <105584041+syflairenicole@users.noreply.github.com>
2024-03-16 03:48:03 -0400

    Update runner.js

--- runner.js
+++ runner.js
@@ -7,6 +7,10 @@ const site = protocol + "//" + domain;
 const logsAPI = "/api/client/servers/(id)/activity?sort=-timestamp&page=(page)&include[]=actor";
 const panelRegex = /^(free|pro)\.pylexnodes.net$/;
 
+if (typeof savingActivityLogs == "undefined") {
+    savingActivityLogs = false;
+};
+
 function jsonSaveToFile(data, fileName) {
     const text = JSON.stringify(data);
     const blob = new Blob([text], {
@@ -63,8 +67,13 @@ async function saveActivityLogs(cursor, limit) {
     const serverId = getServerId();
     if (!serverId) return console.log("Please access your server first.");
 
+    if (savingActivityLogs) return console.warn("Still fetching activity logs, please wait.");
+    savingActivityLogs = true;
+
     const activityLogs = await getActivityLogs(serverId, cursor || 1, limit || 1);
     if (activityLogs) jsonSaveToFile(activityLogs, `pylex-${serverId}-activity`);
+
+    (savingActivityLogs) = false;
 };
 
 let cursor = null;
