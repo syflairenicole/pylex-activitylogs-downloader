9eacc0911fdfdb966291453914c02b77808e4c44
syflairenicole <105584041+syflairenicole@users.noreply.github.com>
2024-03-16 14:38:56 -0400

    Update externalTest2.js

--- tests/externalTest2.js
+++ tests/externalTest2.js
@@ -1,4 +1,6 @@
 // Runs automatically with - https://chrome.google.com/webstore/detail/lmilalhkkdhfieeienjbiicclobibjao
+// Access panel first, then paste & save code, and also check 'Enable On' in the user interface. (To run automatically when you visit the page)
+
 // Add buttons to download activity logs for servers shown on the panel dashboard
 // https://free.pylexnodes.net or https://pro.pylexnodes.net
 
